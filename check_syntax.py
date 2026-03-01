889d7177fec4dbf4bca81fecc443c93a7ccaecab
BlackEagleBCC <apizpudin96@gmail.com>
2026-03-01 10:07:16 +0800

    Remove dev utility scripts from repo

--- check_syntax.py
+++
@@ -1,6 +0,0 @@
-import py_compile
-try:
-    py_compile.compile(r'c:\Users\Desktop\AppData\Local\Programs\Python\Python38-32\PenipuMYV2\PDC\admin_dashboard.py', doraise=True)
-    print('SYNTAX OK')
-except py_compile.PyCompileError as e:
-    print(f'SYNTAX ERROR: {e}')
